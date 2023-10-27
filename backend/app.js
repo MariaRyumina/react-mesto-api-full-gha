0228333d5237f0ade523611fd022a232b27c5a57
Maria Ryumina <maria.ryumin@yandex.ru>
2023-10-27 18:13:33 +0000

    feat: создала сервер, домен

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 // слушаем 3000 порт
 const {
   PORT = 3000,
-  MONGO_URL = 'mongodb://localhost:27017/mestodb',
+  MONGO_URL = 'mongodb://127.0.0.1:27017/mestodb',
 } = process.env;
 
 // создание приложения методом express
