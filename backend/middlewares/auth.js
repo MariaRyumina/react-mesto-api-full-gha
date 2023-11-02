30e43461876a222e65358f44c63d6a79c0fff306
Maria Ryumina <maria.ryumin@gmail.com>
2023-11-03 01:26:37 +0300

    done

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-const { JWT_SECRET } = process.env;
+const { NODE_ENV, JWT_SECRET } = process.env;
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
 module.exports = (req, res, next) => {
